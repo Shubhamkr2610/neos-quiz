8cea2e152cdb889c647f1d818f8060a17a296657
Shubhamkr2610 <shubham1bcabnc18@gmail.com>
2022-04-02 17:46:35 +0530

    created : home page for quiz app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { Footer } from './components/footer/Footer';
+import { Header } from './components/header/Header';
+import { Home } from './pages/home-page/Home';
+import {Quiz} from './pages/quiz-page/Quiz';
+import {Result} from './pages/result-page/Result';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>first react app</h1>
-    </div>
+
+    <BrowserRouter>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" exact element={ <Home /> }/>
+          <Route path="/Quiz" exact element={ <Quiz /> }/>
+          <Route path="/Result" exact element={ <Result /> }/>
+        </Routes>
+        </div>
+        <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+import {Link} from 'react-router-dom'
+
+export const Header = () => {
+  return (
+      <>
+        <div className='header'>
+            <Link to="/" className='brand-title'>
+            Neos- Quiz
+            </Link>
+
+            <Link to='/' className='guide-button'>
+            guidelines
+            </Link>   
+        </div>
+      </>
+  )
+}
